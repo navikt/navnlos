fb7e854758c275e794fbaacb69dd416441a8a552
Mathias.Herbert.Hamre <mathias.herbert.hamre@nav.no>
2024-06-19 14:13:27 +0200

    ping pong

--- src/main/kotlin/no/nav/QuizApplication.kt
+++ src/main/kotlin/no/nav/QuizApplication.kt
@@ -4,6 +4,7 @@ import no.nav.quizrapid.*
 import no.nav.rapid.Answer
 import no.nav.rapid.Assessment
 import no.nav.rapid.Question
+import org.postgresql.core.v3.QueryExecutorImpl
 
 
 /**
@@ -16,6 +17,7 @@ class QuizApplication(private val teamName: String): QuizParticipant(teamName) {
     override fun handle(question: Question) {
         logger.log(question)
         if (question.category == "team-registration") handleRegisterTeam(question)
+        else if (question.category == "ping-pong") handlePingPong(question)
     }
 
 
@@ -30,6 +32,17 @@ class QuizApplication(private val teamName: String): QuizParticipant(teamName) {
     /**
      * Spørsmål handlers
      */
+
+    private fun handlePingPong(question: Question){
+        var answer = ""
+        if (question.question== "ping"){
+            answer = "pong"
+        }else if(question.question == "pong") {
+            answer = "ping"
+        }
+        answer(question.category, questionId = question.id(), answer)
+
+    }
 
     private fun handleRegisterTeam(question: Question) {
         //TODO("Her må du skrive kode ;)")
